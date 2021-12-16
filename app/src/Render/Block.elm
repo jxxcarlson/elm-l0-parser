1c885893147a3c29ea7dd1e6b787be5486e4aa23
James Carlson <jxxcarlson@gmail.com>
2021-12-16 10:59:29 -0500

    tweak error messages

--- app/src/Render/Block.elm
+++ app/src/Render/Block.elm
@@ -203,9 +203,27 @@ renderDisplayMath count settings args id str =
     let
         w =
             String.fromInt settings.width ++ "px"
+
+        lines =
+            String.lines str
+
+        n =
+            List.length lines
+
+        lastLine =
+            List.Extra.getAt (n - 1) lines
     in
-    Element.column [ Events.onClick (SendId id) ]
-        [ Render.Math.mathText count w "id" DisplayMathMode str ]
+    if lastLine == Just "$$" then
+        Element.column [ Events.onClick (SendId id), Element.width (Element.px settings.width), Element.centerX ]
+            [ Render.Math.mathText count w "id" DisplayMathMode (String.join "\n" (List.take (n - 1) lines)) ]
+
+    else if lastLine == Just "$" then
+        Element.column [ Events.onClick (SendId id), Font.color Render.Settings.blueColor ]
+            (List.map Element.text ("$$" :: List.take (n - 1) lines) ++ [ Element.paragraph [] [ Element.text "$", Element.el [ Font.color Render.Settings.redColor ] (Element.text " - another $?") ] ])
+
+    else
+        Element.column [ Events.onClick (SendId id), Font.color Render.Settings.blueColor ]
+            (List.map Element.text ("$$" :: lines) ++ [ Element.el [ Font.color Render.Settings.redColor ] (Element.text "$$??") ])
 
 
 renderCode count settings args id str =
