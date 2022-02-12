42b70f67fad23f1ac5e7ff3680829b4a6697be20
James Carlson <jxxcarlson@gmail.com>
2022-02-12 16:13:01 -0500

    working on editor

--- app/README.md
+++ app/README.md
@@ -4,7 +4,7 @@ script in `scripts.yaml`.
 
 
 
-[Setting up a codemirror 6 editor](https://www.raresportan.com/how-to-make-a-code-editor-with-codemirror6/)
+[How to Make a Code Editor with Codemirror 6 ***](https://www.raresportan.com/how-to-make-a-code-editor-with-codemirror6/)
 
 [Custom Elements for Elm @ Elmprogramming](https://elmprogramming.com/interacting-with-web-components.html)
 
@@ -14,5 +14,6 @@ script in `scripts.yaml`.
 
 [dev-to article on custom elements](h   j )
 
+[jsdelivr](https://www.jsdelivr.com/package/npm/codemirror)
 
 [Ellie](https://gist.github.com/lukewestby/dec1cca157faea7aac7ffe5da3280324)
--- app/editor-prepare/editor.js
+++ app/editor-prepare/editor.js
@@ -27,22 +27,65 @@ let myTheme = EditorView.theme({
 
 }, {dark: true})
 
+
+
+
 class CodemirrorEditor extends HTMLElement { // (1)
 
 
+
+
+//                         log: ['a', 'b', 'c'],
+//                           get latest() {
+//                             if (this.log.length === 0) {
+//                               return undefined;
+//                             }
+//                             return this.log[this.log.length - 1];
+//                           }
+
+//       get editorText() {
+//
+//                  return this.editor.state.doc.toString()
+//           }
+
+//      set value(val) {
+//          this.innerHTML = val
+//          this.dispatchEvent(new CustomEvent("change", {detail: this.innerHTML}))
+//      }
+
+
       // Fires when an instance of the element is created
-      constructor(self) {
-
-        console.log("EDITOR: In constructor")
-
-        self = super(self)
-
-        return self
-    }
+//      constructor(self) {
+//
+//        console.log("EDITOR: In constructor")}
+//
+//
+//        self = super(self)
+//
+//
+//
+//        return self;
+//
+//
+//    }
 
     connectedCallback() {
 
         console.log("EDITOR: In connectedCallback")
+
+
+        const stx = function setText(str, editor) {
+
+         var currentValue = editor.state.doc.toString();
+         var endPosition = currentValue.length;
+
+         editor.dispatch({
+           changes: {
+             from: 0,
+             to: endPosition,
+             insert: str}
+         })}
+
 
         let editor = new EditorView({
             state: EditorState.create({
@@ -53,10 +96,19 @@ class CodemirrorEditor extends HTMLElement { // (1)
                 , EditorView.updateListener.of((v)=> {
                     if(v.docChanged) {
                         console.log( editor.state.doc.toString());
+                        stx("Hello!", editor)
+//                        on("change", (event) => {
+//                                        this.dispatchEvent(new CustomEvent("change", { bubbles: true, composed: true, detail: event }))
+//                                        })
                     }
                   })
                 ],
             doc: "L0 is simple markup language whose syntax is inspired by Lisp. L0 text consists of ordinary text, elements, and blocks.  L0 is simple markup language whose syntax is inspired by Lisp. L0 text consists of ordinary text, elements, and blocks.  L0 is simple markup language whose syntax is inspired by Lisp. L0 text consists of ordinary text, elements, and blocks.  L0 is simple markup language whose syntax is inspired by Lisp. L0 text consists of ordinary text, elements, and blocks."
+
+
+
+
+
 
             }),
             parent: document.getElementById("editor-here")
